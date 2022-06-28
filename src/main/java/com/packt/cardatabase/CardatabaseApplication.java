ef0897d756e61efa8fa8dd66f2ba8c60c56886cc
Arian Omidvar <arian1395omidvar@gmail.com>
2022-06-29 03:19:25 +0430

    - CommandLineRunner

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -2,11 +2,13 @@ package com.packt.cardatabase;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CardatabaseApplication {
+public class CardatabaseApplication implements CommandLineRunner {
+
 
 	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 
@@ -16,4 +18,8 @@ public class CardatabaseApplication {
 		logger.info("Application started");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("command runner is started: ");
+	}
 }
