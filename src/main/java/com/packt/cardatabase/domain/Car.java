b1fe33d009ffc82181b3ad7b9014018f59e7e45b
Arian Omidvar <arian1395omidvar@gmail.com>
2022-07-01 05:35:58 +0430

    - JPA Init

--- src/main/java/com/packt/cardatabase/domain/Car.java
+++ src/main/java/com/packt/cardatabase/domain/Car.java
@@ -1,13 +1,20 @@
 package com.packt.cardatabase.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Car {
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
 	private long id;
 	private String brand, model, color, registerNumber;
-	private int year, price;
-	
+	@Column(name = "PRODUCT_YEAR")
+	private int year;
+	private int price;
+
 	public Car() {}
 	
-	public Car(String brand, String model, String color, 
+	public Car(String brand, String model, String color,
 			String registerNumber, int year, int price) {
 		super();
 		this.brand = brand;
@@ -18,6 +25,7 @@ public class Car {
 		this.price = price;
 	}
 
+	@Transient
 	private Owner owner;
 
 
